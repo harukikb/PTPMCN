f6c2f50623cb70b4ff448a8d1332e472a460c784
Trần Huy <harukikb@gmail.com>
2019-11-01 18:25:29 +0700

    "huy push giao dien"

--- application/views/default/footer.php
+++ application/views/default/footer.php
@@ -2,43 +2,41 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
-                    <h3>Need help?</h3>
-                    <a href="tel://004542344599" id="phone">+45 423 445 99</a>
-                    <a href="mailto:help@citytours.com" id="email_footer">help@citytours.com</a>
+                    <h3>Bạn cần giúp đỡ ?</h3>
+                    <a href="tel://0399002999" id="phone">0399002999</a>
+                    <a href="mailto:teamB1@gmail.com" id="email_footer">teamB1@gmail.com</a>
                 </div>
                 <div class="col-md-3">
-                    <h3>About</h3>
+                    <h3>Về chúng tôi</h3>
                     <ul>
-                        <li><a href="gioi-thieu">About us</a></li>
-                        <li><a href="#">Login</a></li>
-                         <li><a href="#">Terms and condition</a></li>
+                        <li><a href="gioi-thieu">Giới thiệu</a></li>
+                        <li><a href="#">Đăng nhập </a></li>
+                       
                     </ul>
                 </div>
                 <div class="col-md-3">
-                    <h3>Discover</h3>
+                    <h3>Khám phá</h3>
                     <ul>
-                        <li><a href="#">Community blog</a></li>
-                        <li><a href="#">Tour guide</a></li>
-                        <li><a href="#">Wishlist</a></li>
-                         <li><a href="#">Gallery</a></li>
+                        <li><a href="#">Quy Nhơn</a></li>
+                        <li><a href="#">Phú Yên</a></li>
                     </ul>
                 </div>
                 <div class="col-md-2">
-                    <h3>Settings</h3>
+                    <h3>Cài Đặt</h3>
                     <div class="styled-select">
                         <select name="lang" id="lang">
-                            <option value="English" selected>English</option>
-                            <option value="French">French</option>
+                            <option value="VietNamese" selected>Tiếng Việt</option>
+                           <!-- <option value="French">French</option>
                             <option value="Spanish">Spanish</option>
-                            <option value="Russian">Russian</option>
+                            <option value="Russian">Russian</option>-->
                         </select>
                     </div>
                     <div class="styled-select">
                         <select name="currency" id="currency">
-                            <option value="USD" selected>USD</option>
-                            <option value="EUR">EUR</option>
+                            <option value="VNĐ" selected>VNĐ</option>
+                            <!--<option value="EUR">EUR</option>
                             <option value="GBP">GBP</option>
-                            <option value="RUB">RUB</option>
+                            <option value="RUB">RUB</option>-->
                         </select>
                     </div>
                 </div>
@@ -55,7 +53,7 @@
                             <li><a href="#"><i class="icon-vimeo"></i></a></li>
                             <li><a href="#"><i class="icon-youtube-play"></i></a></li>
                         </ul>
-                        <p>© Citytours 2018</p>
+                        <p>© TEAM B1 2019</p>
                     </div>
                 </div>
             </div><!-- End row -->
--- application/views/default/header.php
+++ application/views/default/header.php
@@ -2,11 +2,11 @@
     <div id="top_line">
         <div class="container">
             <div class="row">
-                <div class="col-6"><i class="icon-phone"></i><strong>0045 043204434</strong></div>
+                <div class="col-6"><i class="icon-phone"></i><strong>0399002999</strong></div>
                 <div class="col-6">
                     <ul id="top_links">
-                        <li><a href="#sign-in-dialog" id="access_link">Sign in</a></li>
-                        <li><a href="wishlist.html" id="wishlist_link">Wishlist</a></li>
+                        <li><a href="#sign-in-dialog" id="access_link">Đăng nhập</a></li>
+                        <!--<li><a href="wishlist.html" id="wishlist_link">Wishlist</a></li>-->
                     </ul>
                 </div>
             </div><!-- End row -->
